a776635ed5eb4f72a2f43fd5ed01c1dbd8e713ca
Sara Patel <gallegosara@hotmail.com>
2021-01-18 22:45:07 -0500

    Updated and removed extra prints

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,14 @@ import os
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
-print(csvpath)
+
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     months = 0
@@ -23,7 +20,6 @@ with open(csvpath) as csvfile:
     months_list = []
 
     for row in csvreader:
-        print(row[0],row[1])
         months = months + 1
         pl = int(row[1])
         profit_losses = profit_losses + pl
@@ -33,14 +29,17 @@ with open(csvpath) as csvfile:
     count = 0
     changes = []
     
-    for item in rows :
+    for pl in rows :
+        # Skip 1st data point
         if count != 0: 
-            change =  item - rows[count - 1]
+            change =  pl - rows[count - 1]
             changes.append(change)
+
         count = count + 1
 
     max_changes = max(changes)
     min_changes = min(changes)
+    # Must add 1 to index
     max_month = changes.index(max_changes) + 1
     min_month = changes.index(min_changes) + 1 
 
